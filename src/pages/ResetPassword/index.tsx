9ed0b3e12078a0eb6fa5211febfb896e80dea44f
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-10-14 08:12:01 -0300

    Recuperação de senha

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import {FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { useToast } from '../../hooks/toast';
 import getValidationErros from '../../utils/getValidationErrors';
@@ -13,6 +13,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
@@ -24,6 +25,7 @@ const ResetPassword: React.FC = () => {
 
   const { addToast } = useToast();
   const history = useHistory();
+  const location = useLocation();
 
   const handleSubmit = useCallback(
     async (data: ResetPasswordFormData) => {
@@ -39,6 +41,19 @@ const ResetPassword: React.FC = () => {
         });
         await schema.validate(data, {
           abortEarly: false,
+        });
+
+        const { password, password_confirmation} = data;
+        const token = location.search.replace('?token=', '');
+
+        if(!token){
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
         });
 
         history.push('/');
@@ -57,7 +72,7 @@ const ResetPassword: React.FC = () => {
         });
       }
     },
-    [addToast, history],
+    [addToast, history, location.search],
   );
   return (
     <Container>
@@ -79,7 +94,6 @@ const ResetPassword: React.FC = () => {
               placeholder="Confirmação da senha"
             />
             <Button type="submit">Alterar senha </Button>
-            <Link to="forgot-password"> Esqueci minha senha </Link>
           </Form>
         </AnimationContainer>
       </Content>
