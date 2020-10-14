52ab29f5833c9bbe79fd4d56ce490ca388b20966
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-10-14 10:25:14 -0300

    Criando Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+import { Container, Header, HeaderContent, Profile } from './styles';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => {
+
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src={user.avatar_url}
+              alt={user.name}
+            />
+            <div>
+              <span> Bem Vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
